614f8852ac26395891bf0d402f8bc1ae30048a93
Jinlin <35191548+Anthony2018@users.noreply.github.com>
2022-08-17 18:02:49 -0700

    Add files via upload

--- Pytroch/network_single.py
+++ Pytroch/network_single.py
@@ -30,11 +30,12 @@ class FFT_Conv_Layer(nn.Module):
 
     def __init__(self, imgSize, inCs, outCs1, outCs2, outCs3, imagDim, filtSize, cuda=False):
         super(FFT_Conv_Layer, self).__init__()
-        self.filts1 = np.random.normal(0, 0.01, (1, inCs, outCs1, filtSize, filtSize, imagDim))
-        self.filts2 = np.random.normal(0, 0.01, (1, outCs1, outCs2, filtSize, filtSize, imagDim))
-        self.filts3 = np.random.normal(0, 0.01, (1, outCs2, outCs3, filtSize, filtSize, imagDim))
+        self.filts1 = np.random.normal(0, 0.01, (inCs, outCs1, imagDim, filtSize, filtSize))
+        self.filts2 = np.random.normal(0, 0.01, (outCs1, outCs2, imagDim, filtSize, filtSize))
+        self.filts3 = np.random.normal(0, 0.01, (outCs2, outCs3, imagDim, filtSize, filtSize))
         self.imgSize = imgSize
-        self.filtSize = np.size(self.filts1, 4)
+        self.filtSize = np.size(self.filts1, 3)
+        self.pad = nn.ConstantPad2d(((imgSize-4)//2), 0)
 
         if cuda:
             self.filts1 = torch.from_numpy(self.filts1).type(torch.float32).cuda()
@@ -53,7 +54,7 @@ class FFT_Conv_Layer(nn.Module):
         imgs = imgs.unsqueeze(2)
         imgs = imgs.unsqueeze(5)
 
-        imgs = F.pad(imgs, (0, 0, 0, self.filtSize - 1, 0, self.filtSize - 1))
+        #imgs = F.pad(imgs, (0, 0, 0, self.filtSize - 1, 0, self.filtSize - 1))
         imgs = imgs.squeeze(5)
 
         imgs = torch.rfft(imgs, 2, onesided=False)
@@ -64,9 +65,20 @@ class FFT_Conv_Layer(nn.Module):
         imgsIm = imgs[:, :, :, :, :, 1]
 
         # Pad and transform the filters
-        filts1 = F.pad(self.filts1, (0, 0, 0, self.imgSize - 1, 0, self.imgSize - 1))
-        filts2 = F.pad(self.filts2, (0, 0, 0, self.imgSize - 1, 0, self.imgSize - 1))
-        filts3 = F.pad(self.filts3, (0, 0, 0, self.imgSize - 1, 0, self.imgSize - 1))
+        filts1 = self.pad(self.filts1)
+        filts1shape = filts1.shape
+        filts1 = filts1.view(filts1shape[0],filts1shape[1],filts1shape[3],filts1shape[4],filts1shape[2])
+        filts1 = filts1.unsqueeze(0)
+
+        filts2 = self.pad(self.filts2)
+        filts2shape = filts2.shape
+        filts2 = filts2.view(filts2shape[0], filts2shape[1], filts2shape[3], filts2shape[4], filts2shape[2])
+        filts2 = filts2.unsqueeze(0)
+
+        filts3 = self.pad(self.filts3)
+        filts3shape = filts3.shape
+        filts3 = filts3.view(filts3shape[0], filts3shape[1], filts3shape[3], filts3shape[4], filts3shape[2])
+        filts3 = filts3.unsqueeze(0)
 
         filts1 = torch.fft(filts1, 2)
         filts2 = torch.fft(filts2, 2)
@@ -110,7 +122,7 @@ class FFT_Conv_Layer(nn.Module):
 class StudentNetwork_noRelu(nn.Module):
     def __init__(self,in_channels):
         super(StudentNetwork_noRelu, self).__init__()
-        self.conv1 = FFT_Conv_Layer(imgSize=28, inCs=in_channels, outCs1=32, outCs2=128, outCs3=256, imagDim=2, filtSize=3, cuda=True)
+        self.conv1 = FFT_Conv_Layer(imgSize=28, inCs=in_channels, outCs1=32, outCs2=128, outCs3=256, imagDim=2, filtSize=4, cuda=True)
 
         self.conv2_bn = nn.BatchNorm2d(256)
         self.fc1 = nn.Linear(9216, 512)
@@ -175,14 +187,46 @@ class Teacher_Network(nn.Module):
         forw = self.fc3(forw)
         return self.m(forw)
 
+
+class Student_Network_one_conv(nn.Module):
+    def __init__(self, in_channels):
+        super(Student_Network_one_conv, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=256, kernel_size=6,bias=False)
+
+        self.conv2_bn = nn.BatchNorm2d(256)
+        self.fc1 = nn.Linear(9216, 10)
+
+        self.dropout_input = 0.5
+        self.dropout_hidden = 0.5
+        self.is_training = True
+        self.avepool = nn.AdaptiveAvgPool2d((6, 6))
+        self.m = nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+        # print(x.shape)
+        forw = nn.functional.relu(self.conv1(x))
+        # print(forw.shape)
+
+        forw = self.conv2_bn(forw)
+        forw = self.avepool(forw)
+        forw = forw.view(-1, 9216)
+        # forw = F.dropout(forw, p=self.dropout_input, training=self.is_training)
+        # forw = F.dropout(self.fc1(forw), p=self.dropout_hidden, training=self.is_training)
+        # forw = F.relu(forw)
+        # forw = self.fc2(forw)
+        # forw = F.relu(forw)
+        forw = self.fc1(forw)
+        return self.m(forw)
+
 if __name__ == '__main__':
     # test teacher
-    test_net = Teacher_Network(in_channels=1)
+    test_net = Student_Network_one_conv(in_channels=1)
     x = torch.zeros([16, 1, 28, 28])
-    test_net(x)
-
-    # test student
-    torch.cuda.set_device(1)
-    test_net = StudentNetwork_noRelu().cuda()
-    x = torch.zeros([8, 3, 28, 28]).cuda()
-    test_net(x)
+    out = test_net(x)
+    print(out.shape)
+    #
+    # # test student
+    # torch.cuda.set_device(1)
+    # test_net = Student_Network_one_conv().cuda()
+    # x = torch.zeros([8, 3, 28, 28]).cuda()
+    # test_net(x)
